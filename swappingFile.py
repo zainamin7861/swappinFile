7ef918a1f65b0713f7ef92c7fca0dd97a2e2ff61
zainamin7861 <100061926+zainamin7861@users.noreply.github.com>
2022-05-22 15:06:27 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,27 @@
+def swapFileData():
+    file1 = input("enter file name:- ")
+    file2 = input("enter file name you want to replace with:- ")
+
+    with open(file1, 'r') as a:
+        data_a = a.read()
+
+    with open(file2, 'r') as b:
+        data_b = b.read()
+
+
+
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    
+    with open(file2, 'w') as b:
+        b.write(data_a)    
+
+
+
+swapFileData()    
+
+
+            
+
+    
+    
